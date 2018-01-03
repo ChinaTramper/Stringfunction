344329d2b6bff1d1fb4004b12d0fd97e5c88b0d2
Tramper <wdp924@163.com>
2018-01-03 23:13:34 +0800

    创建字符处理工程

---
+++ include/strfunc.h
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+typedef enum Init_Ret
+{
+	SUCCESS = 0,
+	ERROR_TRACEMODE,
+	ERROR_TRACEFILE,
+	ERROR_NOTDEFINE,
+}Init_Val;
+
+
+
---
+++ include/typedef.h
@@ -0,0 +1,34 @@
+
+#ifndef TRUE
+	#define TRUE 1
+	#define FALSE 0
+#endif
+
+#ifndef NULL
+	#define NULL (void *)0
+#endif
+
+typedef unsigned char UINT8;
+typedef unsigned int UINT32;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
